fc4033800da350c4c392f56a2a635cd7a402c7ec
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-01-16 17:54:27 -0500

    data: begin to get data
    ignore token file for safety

---
+++ get_data.py
@@ -0,0 +1,20 @@
+import os
+from eventbrite import Eventbrite
+
+def get_eventbrite_data():
+    eventbrite = Eventbrite('my-oauth-token')
+    user = eventbrite.get_user()  # Not passing an argument returns yourself
+    print("id: " + user['id'])
+
+def load_oauth_token(current_dir):
+    token_path = "secrets/oauth.token"
+    file_path = os.path.join(current_dir, token_path)
+
+    with open(file_path, "r") as file:
+        return file.read().strip()        
+    
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    print("token: " + load_oauth_token(current_dir))
+    # get_eventbrite_data(load_oauth_token(current_dir))
