9dc8ea706261145a85d0ed9049fba4edb0c25093
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-01-18 14:51:12 -0500

    data: grab tomorrows events (1 page)

--- get_data.py
+++ get_data.py
@@ -1,10 +1,18 @@
 import os
-from eventbrite import Eventbrite
+from requests_html import HTMLSession
 
-def get_eventbrite_data(token):
-    eventbrite = Eventbrite(token)
-    user = eventbrite.get_user()  # Not passing an argument returns yourself
-    print("id: " + user['id'])
+def grab_results_via_url():
+    session = HTMLSession()
+    search_term = "ny--new-york/events--tomorrow/?page=1"
+    url = f"https://www.eventbrite.com/d/online/{search_term}/"
+
+    response = session.get(url)
+    event_list_items = response.html.find('.search-results-panel-content__events section li')
+
+    # Iterate through and print each item
+    for item in event_list_items:
+        print(item.text)
+        print("-" * 30)
 
 def load_oauth_token(current_dir):
     token_path = "secrets/oauth.token"
@@ -12,10 +20,9 @@ def load_oauth_token(current_dir):
 
     with open(file_path, "r") as file:
         return file.read().strip()        
-    
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    token = load_oauth_token(current_dir)
-    # print("token: " + token)
-    get_eventbrite_data(token)
+    # token = load_oauth_token(current_dir)
+
+    grab_results_via_url()
