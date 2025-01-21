325118a55fc1eef413e8e800b6dcc341e218d157
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-01-21 18:53:48 -0500

    grab data

--- get_data.py
+++ get_data.py
@@ -1,18 +1,24 @@
 import os
 from requests_html import HTMLSession
 
-def grab_results_via_url():
+def grab_results_via_url(page_number):
     session = HTMLSession()
-    search_term = "ny--new-york/events--tomorrow/?page=1"
+    search_term = f"ny--new-york/events--tomorrow/?page={page_number}"
     url = f"https://www.eventbrite.com/d/online/{search_term}/"
 
     response = session.get(url)
     event_list_items = response.html.find('.search-results-panel-content__events section li')
 
-    # Iterate through and print each item
+    results = ''
     for item in event_list_items:
-        print(item.text)
-        print("-" * 30)
+        results += '\n'
+        results += "-" * 30
+        results += '\n'
+        results += item.text
+
+    results += ''
+
+    return results
 
 def load_oauth_token(current_dir):
     token_path = "secrets/oauth.token"
@@ -25,4 +31,9 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # token = load_oauth_token(current_dir)
 
-    grab_results_via_url()
+    results = ''
+    for page_number in range(60):
+    # for page_number in range(1):
+        results += grab_results_via_url(page_number)
+    
+    print(results)
