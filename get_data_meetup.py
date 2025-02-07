328fad2855a87f8f100e2226acf268ef3ae73e0a
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-02-06 22:18:58 -0500

    data: print delimted text to file

--- get_data_meetup.py
+++ get_data_meetup.py
@@ -1,8 +1,9 @@
 import os
 import json
 from requests_html import HTMLSession
+from datetime import datetime
 
-def create_query_json(endCursor):
+def create_query_json(endCursor, start_date):
     json_string = '''
 {
   "operationName": "recommendedEventsWithSeries",
@@ -31,6 +32,9 @@ def create_query_json(endCursor):
     json_data = json.loads(json_string)
 
     json_data["variables"]["after"] = endCursor
+    json_data["variables"]["seriesStartDate"] = start_date
+    json_data["variables"]["startDateRange"] = f"{start_date}T00:00:00-05:00"
+    json_data["variables"]["endDateRange"] = f"{start_date}T23:59:00-05:00"
 
     return json_data
 
@@ -46,8 +50,7 @@ def grab_results(json):
     
     return response_json
 
-def get_all_results():
-  query_json = create_query_json("")
+def get_all_results(query_json):
   response_json = grab_results(query_json)
   hasNextPage = response_json['data']['result']['pageInfo']['hasNextPage']
   nextCursor = response_json['data']['result']['pageInfo']['endCursor']
@@ -56,17 +59,38 @@ def get_all_results():
 
   return response_json
 
-def write_to_file(data_dir, json_data): 
+def create_delimted_text_from_json(response_json):
+    edges = response_json['data']['result']['edges']
+    separator = "\n" + "-" * 30 + "\n"
+
+    text_result = ''
+    for edge in edges:
+        item = edge['node']['description']
+        text_result += f"{item}{separator}"
+    
+    return text_result
+
+def write_json_to_file(data_dir, json_data): 
     os.makedirs(data_dir, exist_ok=True)
 
     text_file = os.path.join(data_dir, 'result.json')
     with open(text_file, "w") as file:
         json.dump(json_data, file, indent=4)
 
+def write_text_to_file(data_dir, text_results):
+    os.makedirs(data_dir, exist_ok=True)
+
+    text_file = os.path.join(data_dir, 'text_delimited.txt')
+    with open(text_file, "w") as file:
+        file.write(text_results)
+
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(current_dir, "data", "meetup")
 
-    response_json = get_all_results()    
+    today_date = datetime.today().strftime("%Y-%m-%d")
+    response_json = get_all_results(create_query_json("", today_date))    
+    text_result = create_delimted_text_from_json(response_json)
 
-    write_to_file(data_dir, response_json)
+    write_json_to_file(data_dir, response_json)
+    write_text_to_file(data_dir, text_result)
