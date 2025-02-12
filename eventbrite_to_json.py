e6f5502938f99ce69cc723eb3f015d1587b59002
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-02-11 20:53:48 -0500

    json: convert html to json
    grab the link for event,
    the main image, and the
    title of the event

--- eventbrite_to_json.py
+++ eventbrite_to_json.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import os
+import json
 
 def read_file(file_path):
     text = ''
@@ -8,25 +9,31 @@ def read_file(file_path):
             text += line
     return text
 
-def remove_inline_styles(html_text):
-    soup = BeautifulSoup(html_text, "html.parser")
-    for tag in soup.find_all():
-        if tag.has_attr("style"):
-            del tag["style"]
+def write_to_file(output_file, json_data):
+    with open(output_file, "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
 
-    return str(soup)
+def html_to_json(html_text): 
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-def write_to_file(output_file, text):
-    with open(output_file, "w") as file:
-        file.write(text)
-        
+    data = []
+    for div in soup.find('html').find_all('div', recursive=False):
+        div_data = {
+            "image": div.find('img', src=True)['src'],
+            "link": div.find('a', href=True)['href'],
+            "title": div.find('h3').get_text(strip=True)
+        }
+        data.append(div_data)
+    
+    return data
+
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(current_dir, 'data')
-    unique_html_file = os.path.join(data_dir, 'unique.html')
+    data_dir = os.path.join(current_dir, 'data', 'eventbrite')
+    html_file = os.path.join(data_dir, 'data.html')
     
-    html_text = read_file(unique_html_file)
-    html_without_styles = remove_inline_styles(html_text);
+    html_text = read_file(html_file)
+    json_data = html_to_json(html_text)
 
-    output_file_name = os.path.join(data_dir, 'without_inline_styles.html')
-    write_to_file(output_file_name, html_without_styles)
+    output_file_name = os.path.join(data_dir, 'data.json')
+    write_to_file(output_file_name, json_data)
