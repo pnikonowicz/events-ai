59b65d6edd746d3557ecc90f19cd7c99772ee779
Paul Nikonowicz <paul.nikonowicz@gmail.com>
2025-02-23 16:58:06 -0500

    html: add recemondations to html

--- json_data_to_html.py
+++ json_data_to_html.py
@@ -10,23 +10,37 @@ def json_to_html(items):
             body { background-color: #000; color: #fff; font-family: Arial, sans-serif; }
             ul { list-style-type: none; padding: 0; }
             li { 
-                margin: 10px 0; 
-                display: flex; 
+                margin: 1em 0; 
                 align-items: center; 
                 background: linear-gradient(135deg, #1e3c72, #2a5298); /* Deep blue gradient */
-                padding: 15px;
-                border-radius: 10px;
+                padding: 2em;
+                border-radius: 5em;
                 box-shadow: 2px 2px 10px rgba(255, 255, 255, 0.2);
                 transition: transform 0.2s ease-in-out;
                 transform-origin: left; /* Make the scale pivot from the left side */
+                display: flex;
+            }
+            li div {
+                display: flex;
+                flex-direction: column;
             }
             li:hover { 
-                transform: scale(1.1, 1.1); /* Scale up, moving more to the right */
+                transform: scale(1, 1.1); /* Scale up, moving more to the right */
                 box-shadow: 2px 2px 15px rgba(255, 255, 255, 0.4);
             }
-            img { width: 100px; height: auto; margin-right: 20px; border-radius: 5px; }
+            img { 
+                width: 100px; 
+                height: auto; 
+                margin-right: 20px;
+                border-radius: 5px;
+                object-fit: cover;
+            }
             a { text-decoration: none; color: #fff; font-size: 18px; font-weight: bold; }
             a:hover { text-decoration: underline; color: #ffcc00; }
+            span {
+                    display: flex;
+                    flex-direction: column;
+            }
         </style>
 
     </head>
@@ -39,7 +53,10 @@ def json_to_html(items):
         html_content += f"""
             <li>
                 <img src="{item['image']}" alt="Thumbnail">
-                <a href="{item['link']}" target="about:blank">{item['title']}</a>
+                <div>
+                    <span>because you liked: {item.get('recemondation_source') or "N/A"}<span>
+                    <a href="{item['link']}" target="about:blank">{item['title']}</a>
+                </div>
             </li>
         """
 
@@ -64,9 +81,13 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(current_dir, 'data')
     json_data_file = os.path.join(data_dir, 'unique.json')
+    
+    recemondation_json_file = os.path.join(data_dir, 'recemondations.json')
+    recemondation_json_data = read_json_file(recemondation_json_file)
+    recemondation_html_content = json_to_html(recemondation_json_data)
 
-    json_data = read_json_file(json_data_file)
-    result_html_content = json_to_html(json_data)
+    all_json_data = read_json_file(json_data_file)
+    result_html_content = json_to_html(all_json_data)
 
     html_file = os.path.join(data_dir, "all.html")
-    write_html_to_file(html_file, result_html_content)
+    write_html_to_file(html_file, recemondation_html_content + result_html_content)
