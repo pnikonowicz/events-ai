75e01513b28b6bc75297d8d5d6ae09249b2888a0
Paul Nikonowicz <paul.nikonowicz@gmail.com>
2025-02-28 15:19:24 -0500

    fetch: fix type error
    int is not an array

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from web.json_data_to_html import to_html
 
 if __name__ == "__main__":
     fetch_amount = fetch_eventbrite()
-    print(f"fetched: {len(fetch_amount)} results")
+    print(f"fetched: {fetch_amount} results")
 
     joined_amount = collect_all_data()
     print(f"total data records: {joined_amount}")
