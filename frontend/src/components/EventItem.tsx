7cac79138924bea8db63b7c219125fe72035977c
Chris Sanders <astericky@gmail.com>
2025-03-04 21:17:26 +0000

    add mantine styling library

--- frontend/src/components/EventItem.tsx
+++ frontend/src/components/EventItem.tsx
@@ -1,8 +1,39 @@
+import { Group, Image, List, Stack } from '@mantine/core'
 import { UniqueEvent } from "../types"
 
 function EventItem({ uniqueEvent }: { uniqueEvent: UniqueEvent }) {
   return (
-    <li>{uniqueEvent.title}</li>
+    <List.Item styles={{
+      item: {
+        'display': 'block',
+        'width': '100%',
+        'padding': '1.5rem',
+        'margin': '1rem',
+        'borderRadius': '1rem',
+        'background': 'var(--mantine-color-blue-8)',
+      },
+      itemWrapper: {
+        width: '100%',
+      }
+    }}>
+      <Group component="div" styles={{
+        root: {
+          width: '100%',
+        }
+      }}>
+        <Stack style={{
+          width: '200px',
+        }}>
+          <Image src={uniqueEvent.image} />
+        </Stack>
+        <Stack style={{
+          display: 'flex',
+          justifyContent: 'flex-start'
+        }}>
+          {uniqueEvent.title}
+        </Stack>
+      </Group>
+    </List.Item>
   )
 }
 
