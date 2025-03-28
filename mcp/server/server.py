226d668db5fea01056455962f3ea557c14365ee0
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-03-28 19:30:06 -0400

    mcp: boilerplate for mcp integration
    this will return reccemondations to the mcp client.
    
    currently does not work because of python module
    import issues

--- mcp/server/server.py
+++ mcp/server/server.py
@@ -1,13 +1,44 @@
 # server.py
+import os
 from mcp.server.fastmcp import FastMCP
+from common.paths import Paths
+from bs4 import BeautifulSoup
 
 # Create an MCP server
 mcp = FastMCP("Demo")
 
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def recommendations(number_of_items : int = 10) -> str:
+    """
+        Returns recommendations in html format that have been suggested for the day.
+        This takes an optional argument for how many results to retrieve
+        from recommendations that have been bound to the file system
+        from a pre-fetch operation. The number of arguments must be positive and
+        non-zero.
+
+        For instructions about how to pre-fetch recommendations, please
+        see the README.md
+    """
+
+    html_file = os.paths.join(Paths.DATA_DIR, "all.html")
+    with open(html_file, 'r') as file:
+        html_content = html_file.read()
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    ul = soup.find('ul')
+    list_items = ul.find_all('li')
+
+    top_items = list_items[:number_of_items]
+
+    ul_to_return = soup.new_tag('ul')
+    for item in top_items:
+        ul_to_return.append(item)
+
+    return ul_to_return.prettify()
+        
+
+
+    
 
 
 @mcp.resource("greeting://{name}")
@@ -21,4 +52,5 @@ def echo_prompt(message: str) -> str:
     return f"Please process this message: {message}"
 
 if __name__ == "__main__":
-    mcp.run()
+    # mcp.run()
+    print(recommendations())
