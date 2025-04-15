6c966a9030ff73c01ed108f8363043e1af34f300
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-04-15 19:21:33 -0400

    web: generate html without file
    don't invoke the file system when
    creating html to move towards a
    multi tennant system

--- web/json_data_to_html.py
+++ web/json_data_to_html.py
@@ -96,10 +96,14 @@ def get_recemondation_html_content(recemondation_json_file):
         Logger.warn(f"no recemondation data file found at {recemondation_json_file}, skipping")
         return ""
     
-
-def html_to_file():
+def to_html():
     recemondation_json_file = os.path.join(Paths.DATA_DIR, 'recemondations.json')
     recemondation_html_content = get_recemondation_html_content(recemondation_json_file)
+
+    return recemondation_html_content
+
+def html_to_file():
+    recemondation_html_content = to_html()
 
     html_file = Paths.ALL_HTML
     write_html_to_file(html_file, recemondation_html_content)
--- web/server/main.py
+++ web/server/main.py
@@ -5,19 +5,16 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 from aiohttp import web
 from common.logger import Logger
 from ai.embeddings_to_recommendation_json import extract_recommendation
-from web.json_data_to_html import html_to_file
+from web.json_data_to_html import to_html
 from common.paths import Paths
 
 async def handle(request):
     recommendation_count = extract_recommendation(threshold=.85)
     Logger.log(f"found: {recommendation_count} recommendation(s)")
 
-    html_to_file()
+    recommendation_html = to_html()
     
-    with open(Paths.ALL_HTML, 'r') as file:
-        html_content = file.read()
-
-    return web.Response(text=html_content, content_type='text/html')
+    return web.Response(text=recommendation_html, content_type='text/html')
 
 if __name__ == '__main__':
     app = web.Application()
