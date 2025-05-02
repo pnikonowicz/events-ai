336013c120f4a14f06d62c480bb9d2052d4a8c5d
paul nikonowicz <paul.nikonowicz@gmail.com>
2025-05-02 17:05:37 -0400

    fetch: add fetch job
    this is a seperate job that fectches all
    data that the web server will use
    
    the server no longer performs fecthing

---
+++ web/fetch/main.py
@@ -0,0 +1,48 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+from common.logger import Logger
+from fetch.eventbrite.get_data import fetch as fetch_eventbrite
+from fetch.meetup.get_data import fetch as fetch_meetup
+from fetch.collect import collect_all_data
+from fetch.unique import unique
+from ai.json_data_to_embeddings import data_to_embeddings
+from ai.query_data_to_embedding import query_to_embeddings_from_file
+from common.paths import Paths
+from fetch.target_date import QueryDate
+from common.logger import Logger
+from common.paths import clear_directory
+
+def setup(query_date):
+    Logger.log("clearing data")
+    clear_result = clear_directory(Paths.DATA_DIR)
+    
+    if clear_result: 
+        Logger.log("data cleared")
+    else:
+        Logger.error("data not cleared")
+        exit(1)
+
+
+    fetch_amount = fetch_eventbrite(query_date.eventbrite())
+    Logger.log(f"eventbrite fetched: {fetch_amount} results")
+
+    fetch_amount = fetch_meetup(query_date.meetup())
+    Logger.log(f"meetup fetched: {fetch_amount} results")
+
+    joined_amount = collect_all_data()
+    Logger.log(f"total data records: {joined_amount}")
+
+    dups_removed_amount = unique(.60)
+    Logger.log(f"found {dups_removed_amount} duplicates")
+
+    embeddings_count = data_to_embeddings()
+    Logger.log(f"created {embeddings_count} data embeddings")
+
+    query_embeddings_count = query_to_embeddings_from_file()
+    Logger.log(f"created {query_embeddings_count} query embeddings")
+
+if __name__ == '__main__':
+    Logger.log("global setup")
+    setup(QueryDate.Today)
--- web/server/main.py
+++ web/server/main.py
@@ -5,44 +5,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 from aiohttp import web
 from common.logger import Logger
 from web.json_data_to_html import json_to_html
-from fetch.eventbrite.get_data import fetch as fetch_eventbrite
-from fetch.meetup.get_data import fetch as fetch_meetup
-from fetch.collect import collect_all_data
-from fetch.unique import unique
-from ai.json_data_to_embeddings import data_to_embeddings
-from ai.query_data_to_embedding import query_to_embeddings_from_file
 from ai.embeddings_to_recommendation_json import extract_recommendation
 from ai.embeddings_to_recommendation_json import read_embeddings
 from ai.embeddings_to_recommendation_json import get_previous_events
 from common.paths import Paths
-from fetch.target_date import QueryDate
 from common.logger import Logger
-
-def setup(query_date):
-    if os.path.exists(Paths.DATA_DIR):
-        Logger.log("data already exists. setup not needed")
-        return
-
-    fetch_amount = fetch_eventbrite(query_date.eventbrite())
-    Logger.log(f"eventbrite fetched: {fetch_amount} results")
-
-    fetch_amount = fetch_meetup(query_date.meetup())
-    Logger.log(f"meetup fetched: {fetch_amount} results")
-
-    joined_amount = collect_all_data()
-    Logger.log(f"total data records: {joined_amount}")
-
-    dups_removed_amount = unique(.60)
-    Logger.log(f"found {dups_removed_amount} duplicates")
-
-    embeddings_count = data_to_embeddings()
-    Logger.log(f"created {embeddings_count} data embeddings")
-
-    query_embeddings_count = query_to_embeddings_from_file()
-    Logger.log(f"created {query_embeddings_count} query embeddings")
-
-Logger.log("global setup")
-setup(QueryDate.Today)
 
 original_query_data = get_previous_events(Paths.PREVIOUS_EVENTS)
 query_embeddings = read_embeddings(Paths.QUERY_EMBEDDINGS)
